231da1cf77b24b1071c0e922216cf6bff6b19054
thomas011001 <thomasyacoub321@gmail.com>
2025-11-07 02:43:15 +0200

    feat: add express.json() middleware for parsing JSON requests

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ const app: Application = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(morgan("dev"));
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Working!!");

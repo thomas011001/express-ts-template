b977fb225ed5ff7c83fa2846215d49ed9ad17ca8
thomas011001 <thomasyacoub321@gmail.com>
2025-11-07 02:42:39 +0200

    feat: add morgan middleware for logging HTTP requests

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express, { Application, Request, Response } from "express";
+import morgan from "morgan";
 import dotenv from "dotenv";
 
 dotenv.config();
 const app: Application = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(morgan("dev"));
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Working!!");
